b44e5bcb29058fcc24b38941bcb4395ddf124e8e
Jon Wrona <jon@wrona.dev>
2026-01-25 01:03:22 -0600

    some styles and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [],
+  adapter: cloudflare(),
 });
